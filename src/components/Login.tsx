f3b1da09d325fdb6eb52c2b8b0ed58bb19981465
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2021-06-21 01:45:32 -0500

    login css

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,12 +5,14 @@ const Login = () => {
 	const { login } = useUser();
 	
 	return (
-		<div id="loginCont">
-			<h1 className="login-title">Login</h1>
-			<button onClick={login}>
-				<i className="fas fa-power-off"/>
-			</button>
-		</div>
+		<section id="loginCont">
+			<div className="loginBox">
+				<h1 className="login-title">Login</h1>
+				<button onClick={login}>
+					<i className="fas fa-power-off"/>
+				</button>
+			</div>
+		</section>
 	);
 }
 
