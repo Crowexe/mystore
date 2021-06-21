cb8f9016916351c8f2728c2c89d23cf868fef165
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2021-06-21 00:22:59 -0500

    login modified

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,16 @@
 import { useUser } from "../context/user";
+import "./Login.css";
 
 const Login = () => {
 	const { login } = useUser();
 	
 	return (
-		<>
+		<div>
 			<h1 id="login-title">Login</h1>
 			<button onClick={login}>
 				<i className="fas fa-power-off"/>
 			</button>
-		</>
+		</div>
 	);
 }
 
