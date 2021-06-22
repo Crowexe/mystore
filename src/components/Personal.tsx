e8e661d786f914681246b3c8024097e3510d68b1
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2021-06-22 04:52:52 -0500

    update

--- src/components/Personal.tsx
+++ src/components/Personal.tsx
@@ -1,8 +1,3 @@
-import Loading from "./Loading";
-import { SyntheticEvent, useState } from "react";
-import { useUser } from "../context/user";
-import { firebase, firestore } from "../services/firebase";
-import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const Personal = () => {
 
