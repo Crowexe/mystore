f4acb0f77b3e797ce2a071f1cff43f47817f0e6c
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2021-06-22 06:36:50 -0500

    Update Compra.tsx

--- src/components/Compra.tsx
+++ src/components/Compra.tsx
@@ -1,9 +1,33 @@
 
+import { useState, useEffect } from "react";
+
+
+const initialState = JSON.parse(localStorage.getItem("todos") || "[]");
+
 const Compra = () => {
+
+	const [productos] = useState(initialState);
+
+	useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(productos));
+    }, [productos])
 
 	return (
 		<>
-			<h1>Compras</h1>
+            <h1 className="title">Carrito de compras</h1>               
+                <section id="todo-list">
+       
+                )) : (
+                    <section className="status">
+                        <img src="https://i.imgur.com/OMzIpyl.png" alt="checkedlist"/>
+                        <br></br><h2>Carrito de compras vacia</h2><br></br>
+                        <h3>Agrega los productos de compra</h3>
+                    </section>
+                )
+            </section>
+            <section id="bot">
+            </section>
+    )
 		</>
 	);
 }
