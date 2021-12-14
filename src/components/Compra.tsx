5b259d1e346dd751da85664349a3731d85be71d3
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2021-12-14 17:27:03 -0600

    Update Compra.tsx

--- src/components/Compra.tsx
+++ src/components/Compra.tsx
@@ -1,5 +1,7 @@
 import "./Compra.css";
 import { useState, useEffect } from "react";
+import Productos from './Productos';
+import { firestore } from "../services/firebase";
 
 
 const initialState = JSON.parse(localStorage.getItem("productos") || "[]");
@@ -12,14 +14,31 @@ const Compra = () => {
         localStorage.setItem("productos", JSON.stringify(productos));
     }, [productos])
 
+	const findProd = async (id:string) => {
+		const productosRef = firestore.collection('productos').doc(id);
+		const prod = await productosRef.get();
+		if (!prod.exists) {
+			console.log('No such document!');
+		} 
+		else {
+			console.log('Document data:', prod.data());
+		}
+	}
+
+
+
 	return (
 		<>
-            <h1 className="titleCarrito">Carrito de compras</h1>
-			<section id="busquedaCont">
-				<input className="busqueda" type="text" placeholder="Digita el ID del producto"/>
-				<button className="buscar"><i className="fas fa-search"/></button>
-            </section>               
-			<section id="todo-list">	
+            <h1 className="titleCarrito">Carrito de compras</h1>	
+				<section id="busquedaCont">
+					<input className="busqueda" type="text" placeholder="Digita el ID del producto"/>
+					<button className="buscar" onClick={() => findProd(id)}><i className="fas fa-search"/></button>
+				</section> 	
+				<section id="find-prod">
+					
+				</section>      
+			<section id="prod-list">
+				
 				<section className="status">
 					<img src="https://i.imgur.com/OMzIpyl.png" alt="checkedlist"/>
 					<br/><h2 className="estadoCarrito">Carrito de compras vacia</h2>
